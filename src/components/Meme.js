07c1f00e55b83469e8f2b6d9aab1520e1648fd85
Anton Zabulonov <zabulonov.a@gmail.com>
2022-06-17 13:12:05 +0300

    Add random image generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,23 @@
+import memesData from '../memesData';
+import React from 'react';
+
 export default function Meme() {
+	const [memeImage, setMemeImage] = React.useState('');
+
+	function generateMemeImg() {
+		const memeArray = memesData.data.memes;
+		const randomMeme = Math.floor(Math.random() * memeArray.length);
+		setMemeImage(memeArray[randomMeme].url);
+	}
+
 	return (
 		<main>
 			<div className="form">
 				<input type="text" id="first-input" placeholder="Top text" />
 				<input type="text" id="second-input" placeholder="Bottom text" />
-				<button>Get a new meme image 🖼</button>
+				<button onClick={generateMemeImg}>Get a new meme image 🖼</button>
 			</div>
+			<img src={memeImage} alt="" />
 		</main>
 	);
 }
