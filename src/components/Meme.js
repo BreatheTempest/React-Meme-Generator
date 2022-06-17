c2e15de173a11ea4e625c8e99b5aa80bd4aa63f4
Anton Zabulonov <zabulonov.a@gmail.com>
2022-06-17 09:05:27 +0300

    Add responsiveness to the form

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,11 @@
 export default function Meme() {
 	return (
-		<section className="form">
-			<input type="text" name="" id="first-input" placeholder="Shut up" />
-			<input
-				type="text"
-				name=""
-				id="second-input"
-				placeholder="and take my money"
-			/>
-			<button>Get a new meme image 🖼</button>
-		</section>
+		<main>
+			<div className="form">
+				<input type="text" id="first-input" placeholder="Top text" />
+				<input type="text" id="second-input" placeholder="Bottom text" />
+				<button>Get a new meme image 🖼</button>
+			</div>
+		</main>
 	);
 }
